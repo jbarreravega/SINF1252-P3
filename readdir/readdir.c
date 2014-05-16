3b41ea7795906f70355f8b3e5305683bc07f76fe
Juan Sebastian Barrera Vega <juan.barreravega@student.uclouvain.be>
2014-05-16 13:10:07 +0200

    pierrick et je

--- readdir/readdir.c
+++ readdir/readdir.c
@@ -1,158 +1,143 @@
-//author :  Juan Sebastian Barrera Vega (94471000)
-//          Olivier Boegaerts (44481100)
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <err.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
 
 #include "benchmark.h"
 
-#define MAX_FILES 2048
-#define MAX_SIZE 1024// 1KB
-
-int deleteTMP(const char *path, const char *dirName)
+#define N 1000
+/**
+ * \brief Écris le temps `time` en correspondance avec `Nfiles`
+ *
+ * L'`overhead` est d'abord retiré de `time`
+ * En cas d'erreur, il `exit` avec `EXIT_FAILURE`
+ *
+ * \param rec le `recorder` dans lequel écrire, il est supposé non-`NULL`
+ * \param repertoire le nom du repertoire parcouru. 
+ * \param Nfiles l'abscisse
+ * \param t le temps à écrire en ordonnée
+ */
+void benchmark_readdir(timer *t, char *repertoire, recorder *rec, int Nfiles)
 {
-	errno = 0;
-	DIR *dirErase = opendir(dirName);
-	if (errno =! ENOENT) return EXIT_FAILURE;
-	struct dirent *files;
-	while((files=readdir(dirErase)) != NULL)
+	DIR *rep=NULL;
+	rep=opendir(repertoire);
+
+	if(rep==NULL)
 	{
-		if (strcmp(files->d_name, ".") != 0  && strcmp(files->d_name, "..") != 0)
+		fprintf(stderr, "Erreur ouverture dossier source");
+		exit(1);
+	}
+
+	struct dirent* fichierLu=NULL;  
+    start_timer(t);
+	while((fichierLu = readdir(rep)) != NULL)
+	{
+		/*if (strcmp(fichierLu->d_name, ".")!=0 && strcmp(fichierLu->d_name, "..")!=0)
 		{
-			char filename[14];
-			strcpy(filename,path);
-			strcat(filename,files->d_name);
-			remove(filename);
-		}
+			
+		}*/
+	}			
+
+    write_record_n(rec, Nfiles, stop_timer(t), /*Nfiles+2*/ 1);
+
+	if(closedir(rep) == -1)
+	{
+		fprintf(stderr, "Erreur fermeture dosssier source");
+		exit(-1);
 	}
-	if(errno == 0){
-			closedir(dirErase);
-			rmdir(dirName);
-			if (errno !=0){
-				perror("unlink");
-				exit(EXIT_FAILURE);
-			}
-			else return EXIT_SUCCESS;
-	}
-	else{
-		perror("readdir");
-		exit(EXIT_FAILURE);
-	}
+
 }
 
-int createFile(const int fileName, const char *directory, const int nBytes)
+char* stringConcat(char* s1, char* s2)
+ { 
+	 int size = strlen(s1)+strlen(s2);
+	 char* s = (char*)malloc(sizeof(char)*(size+1));
+	 
+	 int j,k, l =0;
+	 for(j=0; j<strlen(s1); ++j)
+	 {
+		 s[j] = s1[j];
+	 }
+	 for(k=j; k<size+1; ++k)
+	 {
+		 s[k] = s2[l];
+		 ++l;
+	 }
+	 
+	 return s;
+ }
+
+void rm(char* repertoire)
 {
-	int i;
-	ssize_t len;
-	char filename[14];
-	FILE *fd;
-	strcpy(filename,directory); // filename = "directory"
-	strcat(filename,"/"); //filename = "directory/"
-	sprintf(filename+5,"%d",fileName); //filename = "directory/fileName
-	fd = fopen(strcat(filename,".dat"),"w+");//open filename = "directory/i.dat"
-	fclose(fd);
-	int file = open (filename, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
-  char *s = (char *) malloc((nBytes+1) * sizeof(char));
-  memset(s, 0, nBytes+1);
-  len = write(file, (void *) s, nBytes);
-  free(s);
-  if (len == -1) {
-  	perror("write");
-  	return EXIT_FAILURE;
-  }
-  close(file);
-	return EXIT_SUCCESS;
-}
-
-int main (int argc, char *argv[])  {
-	timer *t = timer_alloc();
-	recorder *readdirNoSize_rec = recorder_alloc("readdir1.csv");
-	recorder *readdirConstSize_rec = recorder_alloc("readdir2.csv");
-	recorder *readdirVarSize_rec = recorder_alloc("readdir3.csv");
-
-	struct dirent *file;
-	struct dirent *file2;
-	struct dirent *file3;
-	const char *pathdir1 = "tmp1";
-	const char *pathdir2 = "tmp2";
-	const char *pathdir3 = "tmp3";
-	errno = 0;
-
-	mkdir(pathdir1,S_IRWXU);
-	mkdir(pathdir2,S_IRWXU);
-	mkdir(pathdir3,S_IRWXU);
-
-	int j;
-	int numberFiles = 0;
-	for (j = 1 ; j <= MAX_FILES; j *= 2) {
-
-		numberFiles++;
-		//Création d'un fichier
-		createFile(j, pathdir1, 0);
-		createFile(j, pathdir2, MAX_SIZE);
-		createFile(j, pathdir3, j);
-
-		//j fichiers de taille 0
-		DIR *dirp = opendir(pathdir1);
-		if (dirp == NULL) { //Erreur à l'ouverture du dossier
-			perror("opendir pathdir1");
-			exit(EXIT_FAILURE);
-		}
-		start_timer(t);
-		do { file = readdir(dirp);
-		} while (file != NULL);
-		//write_record(readdirNoSize_rec, numberFiles, stop_timer(t));
-		write_record_n(readdirNoSize_rec, numberFiles, stop_timer(t), numberFiles); //moyenne
-		closedir(dirp);
-
-		//j fichiers de même taille (différente de 0)
-		dirp = opendir(pathdir2);
-		if (dirp == NULL) { //Erreur à l'ouverture du dossier
-			perror("opendir pathdir2");
-			exit(EXIT_FAILURE);
-		}
-		start_timer(t);
-		do {file = readdir(dirp);
-		} while (file != NULL);
-		//write_record(readdirConstSize_rec, numberFiles, stop_timer(t));
-		write_record_n(readdirConstSize_rec, numberFiles, stop_timer(t), numberFiles);
-		closedir(dirp);
-
-
-		//j fichiers de taille différente
-		dirp = opendir(pathdir3);
-		if (dirp == NULL) { //Erreur à l'ouverture du dossier
-			perror("opendir3 pathdir3");
-			exit(EXIT_FAILURE);
-		}
-		do {file = readdir(dirp);
-		} while (file != NULL);
-		//write_record(readdirVarSize_rec, numberFiles, stop_timer(t));
-		write_record_n(readdirVarSize_rec, numberFiles, stop_timer(t), numberFiles);
-		closedir(dirp);
-
-		if (errno !=0) {
-			perror("readdir");
-			exit(EXIT_FAILURE);
-		}
+	DIR *rep=NULL;
+	rep=opendir(repertoire);
+	if(rep==NULL)
+	{
+		fprintf(stderr, "Erreur ouverture dossier source");
+		exit(1);
 	}
 
-	recorder_free(readdirNoSize_rec);
-	recorder_free(readdirConstSize_rec);
-	recorder_free(readdirVarSize_rec);
-	timer_free(t);
+	struct dirent* fichierLu=NULL;  
+	while((fichierLu = readdir(rep)) != NULL)
+	{
+		if (strcmp(fichierLu->d_name, ".")!=0 && strcmp(fichierLu->d_name, "..")!=0)
+		{
+			char *s;
+			s = stringConcat("dir/",fichierLu->d_name);
+			remove(s);
+			free(s);
+		}
+	}			
 
-	//Efface les dossiers temporaires
-	deleteTMP("tmp1/",pathdir1);
-	deleteTMP("tmp2/",pathdir2);
-	deleteTMP("tmp3/",pathdir3);
+	if(closedir(rep) == -1)
+	{
+		fprintf(stderr, "Erreur fermeture dosssier source");
+		exit(-1);
+	}
+	
+	rmdir(repertoire);
 
-	return EXIT_SUCCESS;
+}
+
+int main (int argc, char *argv[]) {
+  	  
+  timer *t = timer_alloc();
+
+  recorder *readdir_rec = recorder_alloc("readdir.csv");
+  
+  mkdir("dir", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  char str[10];
+  char* s;
+  char* s1;
+  int filename;
+  for(filename = 1; filename <= N; ++filename)
+  { 
+
+	  sprintf(str,"%d",filename);
+	  s = stringConcat("dir/test",str);
+	  s1 = stringConcat(s,".txt");
+	  //printf("%s\n",s1);
+	  FILE *file = fopen(s1, "w");
+	  if (file == NULL) 
+	  {
+		  perror("fopen");
+		  exit(EXIT_FAILURE);
+	  }
+	  
+	  //fprintf(file, "testtesttesttesttestBENCHMARKREADDDIRMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+      fclose(file);
+	  free(s);
+      free(s1);
+      
+      update_overhead();   
+      benchmark_readdir(t, "dir", readdir_rec, filename);
+	}
+  rm("dir");
+  recorder_free(readdir_rec);
+
+  timer_free(t);
+  return EXIT_SUCCESS;
 }
--- writev/writev.c
+++ writev/writev.c
@@ -1,104 +1,253 @@
-//author :  Juan Sebastian Barrera Vega (94471000)
-//          Olivier Boegaerts (44481100)
+/**
+	\file writev.c
+	\brief Ce programme compare l'utilisation de writev ou de write + lsekk pour ecrire dans un fichier
+
+	Ce programme va donc écrire dans un fichier avec writev d'une part et write + lseek d'autre part, en comparant les temps nécessaires à chacun.	
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/mman.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <sys/uio.h>
-#include <limits.h>
+# include <sys/uio.h>
+#include <string.h>
 
 #include "benchmark.h"
 
-#define MAX_SIZE  0x100000 // 1MB
+#define N 100
 
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
-int main (int argc, char *argv[])
+/*
+ * 
+ * timer *t = chrono 
+ * recorder *write_rec =
+ * char *s = chaine de caractères à écrire
+ * int write_o = addresse du fichier
+ * 
+ */
+
+void benchmark_write (timer *t, recorder *write_rec, char *s, int write_o) 
 {
+	start_timer(t);
+	int size_s = strlen(s);
+	int write_e = write(write_o,s,size_s);
+	if (write_e == -1)
+	{
+		perror("ecriture");
+		exit(EXIT_FAILURE);
+	}
+		
+	//ecriture des résultats dans write.csv 
+	write_record_n(write_rec, size_s, stop_timer(t), 1);
+
+}
+
+int main (int argc, char *argv[])  {
+	
+	int iovcnt;
+	char* S_init = "test"; //String d'écriture initiale.
+	int size_s = strlen(S_init);
+	int size = size_s*N; 
+	
+	char buffer[size+1];
+	buffer[0] = '\0';
+	
+	struct iovec iov[1];
+	char *buf0;
+	char *s;
+	
+	char *buf[N];
+	struct iovec iov1[3];
+	
+	// Déclare un timer, ainsi que 4 recorder qui vont contenir les résultats de l'exécution du programme
 	timer *t = timer_alloc();
 	recorder *writev_rec = recorder_alloc("writev.csv");
-	recorder *write_rec = recorder_alloc("write.csv");
+	recorder *writev_rec2 = recorder_alloc("writev2.csv");
+ 	recorder *write_rec = recorder_alloc("write.csv");
+ 	recorder *write_rec2 = recorder_alloc("write2.csv");
+ 	ssize_t bytes_written;
 
-	int size;
-	int fildes = open("tmp.dat", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
-	ssize_t len;
-	off_t lsk;
-	if(fildes == -1)
+	
+	int write_o = open("write.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+	if (write_o == -1)
 	{
 		perror("open");
 		exit(EXIT_FAILURE);
 	}
-
-	char *s = (char *) malloc((MAX_SIZE + 1) * sizeof(char));
-	//char *s2 = (char *) malloc((MAX_SIZE + 1) *10* sizeof(char));
-	memset(s, 0, MAX_SIZE + 1);
-
-	const int numVec;
-
-	struct iovec* iovectors = malloc(10 * sizeof (struct iovec));
-	if(iovectors == NULL)
-	{
-		perror("malloc");
-		return EXIT_FAILURE;
-	}
-	int i;
-	for (i=0; i<10;i++)
-	{
-		iovectors[i].iov_base=(void *) s;//creation de 10 buffers
-	}
-
-
-	for (size = 1; size <= MAX_SIZE; size *=2 )
-	{
-
-	int j;
-	for (j=0; j<10;j++)
-	{
-		iovectors[j].iov_len=size;//les buffers a ecrire sont de taille size
-	}
-		start_timer(t);
-		int k;
-		len = writev(fildes, iovectors, 10);
-		write_record(writev_rec, size, stop_timer(t));//on ecrit les 10 buffers en une fois
-		if (len == -1)
-		{
-			perror("writev");
-			return EXIT_FAILURE;
-		}
-
-		start_timer(t);
-		int h;
-		for (h=0; h<10; h++)
-		{
-			//lsk = lseek(fildes,0,SEEK_SET);
-			len = write(fildes, (void *) s, size);//on ecrit 10 fois un buffer de taille size
-		}
-		write_record(write_rec, size, stop_timer(t));
-		if (lsk < 0)
-		{
-			perror("lseek");
-			return EXIT_FAILURE;
-		}
-		if (len == -1)
-		{
-			perror("write");
-			return EXIT_FAILURE;
-		}
-
-	}
-	free (iovectors);
-	int err = close(fildes);
-	if(err == -1)
-	{
-		perror("close");
+	
+	int fd = open("writev.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+    if (fd == -1)
+    {
+		perror("open");
 		exit(EXIT_FAILURE);
 	}
-	free(s);
+	
+	// Comparaison du temps pris par write et writev, nous n'utilisons qu'un seul buffer pour les 2 fonctions.
+	int i;
+	for(i = 1; i <= N; ++i)
+	{
+
+		s = strcat(buffer,S_init);
+		
+		//////////////////////////////// fichier write : une écriture unique de i mots//////////////////////////////////////////////////////
+		
+		benchmark_write (t,write_rec,s,write_o);
+		
+       //////////////////////////////////////// fichier writev : utilisation d'un seul buffer pour une écriture de i mots//////////////////
+		
+		
+		buf0 = (char *)realloc(buf0, sizeof(char)*i*size_s);
+		int k1 = 0;
+		int k;
+		for(k=(size_s+1)*(i-1); k < size_s*i; ++k)
+		{
+			buf0[k] = S_init[k1];
+			k1++;
+		}
+		
+		buf0 = (char *)realloc(buf0, (sizeof(char)*(i)*size_s)+sizeof(char));
+		buf0[k+1] = '\0';
+		
+		iov[0].iov_base = buf0;
+		iov[0].iov_len = strlen(buf0);
+		
+		
+		iovcnt = sizeof(iov) / sizeof(struct iovec);
+		start_timer(t);
+		
+		bytes_written = writev(fd, iov, iovcnt);
+		write_record_n(writev_rec, i*size_s, stop_timer(t), 1);
+		if(bytes_written ==-1)
+		{
+			perror("ecriture");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	int write_f = close(write_o);
+	int writev_f = close(fd);
+	if (write_f == -1|| fd ==-1)
+	{
+		perror("fermeture");
+		exit(EXIT_FAILURE);
+	}
+	
+	//free(s);
+	free(buf0);
 	recorder_free(writev_rec);
 	recorder_free(write_rec);
+		
+	// Comparaison du temps pris par write et writev, nous n'utilisons qu'un seul buffer pour write et plusieurs buffer pour writev.
+	for(i = 1; i <= N; ++i)
+	{
+
+		////////////////////////////////// fichier write : i écritures de un mot////////////////////////////////////////////
+	
+				
+		// ouverture fichier
+		int write_o2 = open("write2.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+		if (write_o2 == -1)
+		{
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
+		
+		start_timer(t);
+		
+		int j;
+		for (j=0;j<=i;++j)
+		{
+			int write_e = write(write_o2,S_init,size_s);
+			if (write_e == -1)
+			{
+				perror("ecriture");
+				exit(EXIT_FAILURE);
+			}
+			write_e = write(write_o2,S_init,size_s);
+			if (write_e == -1)
+			{
+				perror("ecriture");
+				exit(EXIT_FAILURE);
+			}
+			write_e = write(write_o2,S_init,size_s);
+			if (write_e == -1)
+			{
+				perror("ecriture");
+				exit(EXIT_FAILURE);
+			}
+		}
+		
+		// écriture du résultat dans write2.csv
+		write_record_n(write_rec2, 3*i*(size_s), stop_timer(t), 1);
+		
+		// fermeture du fichier write2.txt
+		int write_f1 = close(write_o2);
+		if (write_f1 == -1)
+		{
+		perror("fermeture");
+		exit(EXIT_FAILURE);
+		}
+		
+		
+		//////////////////////////////////////// fichier writev : utilisation multi buffer pour l'écriture de i mots////////////////////////////////////////////
+
+		int k;
+	    for(k=0; k <= 3; ++k)
+	    {
+			buf[k] = (char *)malloc(sizeof(char)*(size_s+1));
+			for(j=0; j <= size_s+1; ++j)
+		    {
+			   buf[k][j] = S_init[j];
+		    }
+		    iov1[k].iov_base = buf[k];
+		    iov1[k].iov_len = strlen(buf[k]);
+		}
+		
+		iovcnt = sizeof(iov1) / sizeof(struct iovec);
+		
+		int fd2 = open("writev2.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+        if (fd2 == -1)
+        {
+		    perror("open");
+		   exit(EXIT_FAILURE);
+	     }
+		
+		start_timer(t);
+		for (j=0;j<=i;++j)
+		{
+		    bytes_written = writev(fd2, iov1, iovcnt);
+		    if(bytes_written ==-1)
+		    {
+			    perror("ecriture");
+			    exit(EXIT_FAILURE);
+		    }
+		}
+		
+		write_record_n(writev_rec2, 3*i*size_s, stop_timer(t), 1);
+		
+		if(close(fd2) == -1)
+		{
+			perror("fermeture");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	remove("write.txt");
+	remove("write2.txt");
+	remove("writev.txt");
+	remove("writev2.txt");
+	for(i=0; i <= 3; ++i)
+	{
+		free(buf[i]);
+	}
+	
+	// liberation de la mémoire
+	
+  	recorder_free(writev_rec2);
+  	recorder_free(write_rec2);
 	timer_free(t);
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
+
